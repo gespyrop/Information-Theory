e8fc9085d1798e0af9e8dd7b9f43e88ccbc55c44
gespyrop <giwrgosspyropoulos@gmail.com>
2019-07-02 14:56:15 +0300

    lz78 compression done

--- lz78.py
+++ lz78.py
@@ -1,5 +1,21 @@
 def compress(text):
-	pass
+	dictionary = {'' : 0}
+
+	compressed = ''
+	w = ''
+	for c in text:
+		if w + c in dictionary:
+			w = w + c
+		else:
+			compressed += '({},{})'.format(str((dictionary[w])), c)
+			dictionary[w + c] = len(dictionary)
+			w = ''
+
+	if w != '':
+		compressed += '({})'.format(str((dictionary[w])), c)
+
+	return compressed
+
 
 def decompress(compressed):
-	pass	
+	pass
