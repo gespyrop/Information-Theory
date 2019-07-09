1e48d150227dc43bd4ba230fa0842a9735decaee
gespyrop <giwrgosspyropoulos@gmail.com>
2019-07-09 14:17:43 +0300

    added encodeWord and decodeWord methods

--- linear_code.py
+++ linear_code.py
@@ -3,8 +3,20 @@ from sage.coding.channel_constructions import random_error_vector
 from random import randrange
 
 class LC():
-	def __init__(self):
-		pass
+	def __init__(self, P):
+		P = Matrix(P)
+		G = Matrix(GF(2), identity_matrix(P.nrows()).augment(P))
+		self.C = LinearCode(G)
+
+	#Encodes a binary string with length equal to the code's dimension
+	def encodeWord(self, word):
+		encoded = self.C.encode(vector([int(bit) for bit in list(word)]))
+		return ''.join(str(bit) for bit in encoded)
+
+	#Decodes a binary string with length equal to the code's length
+	def decodeWord(self, word):
+		decoded = self.C.decode_to_message(vector([int(bit) for bit in list(word)]))
+		return ''.join(str(bit) for bit in decoded)
 
 	@staticmethod
 	def getRandomNoise(length, noise_v):
