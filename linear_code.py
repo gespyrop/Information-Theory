975f25ace280eaaaf99112e66f299133ab2b44e2
gespyrop <giwrgosspyropoulos@gmail.com>
2019-07-09 15:16:16 +0300

    encoding/decoding done

--- linear_code.py
+++ linear_code.py
@@ -3,20 +3,61 @@ from sage.coding.channel_constructions import random_error_vector
 from random import randrange
 
 class LC():
-	def __init__(self, P):
+	def __init__(self, P, maxNoise):
 		P = Matrix(P)
 		G = Matrix(GF(2), identity_matrix(P.nrows()).augment(P))
 		self.C = LinearCode(G)
+		self.maxNoise = maxNoise
+
+	#Encoding given binary
+	def encode(self, original):
+
+		#Adding zeros to the end
+		while len(original) % self.C.dimension() != 0:
+			original += '0'
+
+		words = self.getWords(original, self.C.dimension())
+
+		encoded = ''
+
+		for word in words:
+			encoded += self.encodeWord(word, self.maxNoise)
+
+		return encoded
+
+	#Encoding given binary
+	def decode(self, encoded):
+		words = self.getWords(encoded, self.C.length())
+
+		decoded = ''
+
+		for word in words:
+			decoded  += self.decodeWord(word)
+
+		return decoded
+
 
 	#Encodes a binary string with length equal to the code's dimension
-	def encodeWord(self, word):
+	def encodeWord(self, word, maxNoise):
 		encoded = self.C.encode(vector([int(bit) for bit in list(word)]))
+		encoded += LC.getRandomNoise(len(encoded), maxNoise) #Applying noise to word
 		return ''.join(str(bit) for bit in encoded)
 
 	#Decodes a binary string with length equal to the code's length
 	def decodeWord(self, word):
 		decoded = self.C.decode_to_message(vector([int(bit) for bit in list(word)]))
 		return ''.join(str(bit) for bit in decoded)
+
+	#Breaks a binary into words of given length
+	def getWords(self, binary, length):
+		words = []
+
+		for i in range(len(binary) / length):
+			word = binary[length * i : length * (i + 1)]
+			words.append(word)
+
+		return words
+
 
 	@staticmethod
 	def getRandomNoise(length, noise_v):
